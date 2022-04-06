64488999a653d3ba79fc9c339dc6274017895cf0
beebright <william.shi@104.com.tw>
2022-04-06 18:29:07 +0800

    去除相同的if 判斷，提升到迴圈一開始判斷，並跳出迴圈

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,9 @@ class GildedRose {
 
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
+			if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+				continue;
+			}
 			if (items[i].name.equals("Aged Brie")
 					|| items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 				if (items[i].quality < 50) {
@@ -30,17 +33,11 @@ class GildedRose {
 				}
 			} else {
 				if (items[i].quality > 0) {
-					if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-					} else {
-						items[i].quality = items[i].quality - 1;
-					}
+					items[i].quality = items[i].quality - 1;
 				}
 			}
 
-			if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-			} else {
-				items[i].sellIn = items[i].sellIn - 1;
-			}
+			items[i].sellIn = items[i].sellIn - 1;
 
 			if (items[i].sellIn < 0) {
 				if (items[i].name.equals("Aged Brie")) {
@@ -52,10 +49,7 @@ class GildedRose {
 						items[i].quality = items[i].quality - items[i].quality;
 					} else {
 						if (items[i].quality > 0) {
-							if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-							} else {
-								items[i].quality = items[i].quality - 1;
-							}
+							items[i].quality = items[i].quality - 1;
 						}
 					}
 				}
