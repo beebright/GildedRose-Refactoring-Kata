0ec5731213bbf1266513d83f9c45d05ecc6e8647
beebright <william.shi@104.com.tw>
2022-05-05 13:44:16 +0800

    移動 屬於 Item類別的方法

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,70 +1,25 @@
 package com.gildedrose;
 
 class GildedRose {
-	Item[] items;
+  Item[] items;
 
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
 
-	public void updateQuality() {
-		for (Item item : items) {
-			if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-				continue;
-			}
+  public void updateQuality() {
+    for (Item item : items) {
+      if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        continue;
+      }
 
-			if (item.name.equals("Aged Brie")) {
-				updateAgedBrie(item);
-			} else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				updateBackstage(item);
-			} else {
-				updateNormalItem(item);
-			}
-		}
-	}
-
-	private void updateNormalItem(Item item) {
-		item.sellIn = item.sellIn - 1;
-		decreaseQuality(item);
-		if (item.sellIn < 0) {
-			decreaseQuality(item);
-		}
-	}
-
-	private void updateBackstage(Item item) {
-		item.sellIn = item.sellIn - 1;
-		increaseQuality(item);
-
-		if (item.sellIn < 10) {
-			increaseQuality(item);
-		}
-
-		if (item.sellIn < 5) {
-			increaseQuality(item);
-		}
-
-		if (item.sellIn < 0) {
-			item.quality = 0;
-		}
-	}
-
-	private void updateAgedBrie(Item item) {
-		item.sellIn = item.sellIn - 1;
-		increaseQuality(item);
-		if (item.sellIn < 0) {
-			increaseQuality(item);
-		}
-	}
-
-	private void decreaseQuality(Item item) {
-		if (item.quality > 0) {
-			item.quality = item.quality - 1;
-		}
-	}
-
-	private void increaseQuality(Item item) {
-		if (item.quality < 50) {
-			item.quality = item.quality + 1;
-		}
-	}
+      if (item.name.equals("Aged Brie")) {
+        item.updateAgedBrie();
+      } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        item.updateBackstage();
+      } else {
+        item.updateNormalItem();
+      }
+    }
+  }
 }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -2,20 +2,65 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+  public String name;
 
-    public int sellIn;
+  public int sellIn;
 
-    public int quality;
+  public int quality;
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
+  public Item(String name, int sellIn, int quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+
+  @Override
+  public String toString() {
+    return this.name + ", " + this.sellIn + ", " + this.quality;
+  }
+
+  protected void increaseQuality() {
+    if (quality < 50) {
+      quality = quality + 1;
+    }
+  }
+
+  protected void decreaseQuality() {
+    if (quality > 0) {
+      quality = quality - 1;
+    }
+  }
+
+  protected void updateAgedBrie() {
+    sellIn = sellIn - 1;
+    increaseQuality();
+    if (sellIn < 0) {
+      increaseQuality();
+    }
+  }
+
+  protected void updateBackstage() {
+    sellIn = sellIn - 1;
+    increaseQuality();
+
+    if (sellIn < 10) {
+      increaseQuality();
     }
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+    if (sellIn < 5) {
+      increaseQuality();
     }
+
+    if (sellIn < 0) {
+      quality = 0;
+    }
+  }
+
+  protected void updateNormalItem() {
+    sellIn = sellIn - 1;
+    decreaseQuality();
+    if (sellIn < 0) {
+      decreaseQuality();
+    }
+  }
 }
